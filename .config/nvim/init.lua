38d9e082dd6ebe28da7844398a0dd35e3b9bfb0b
Kenneth Heeney <kenneth@heeney.ie>
2026-04-05 19:30:31 +0100

    update nvim-surround repo

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -157,7 +157,7 @@ local plugins = {
         end
     },
     {
-        "kylchui/nvim-surround",
+        "kylechui/nvim-surround",
         version = "^4.0.0", -- Use for stability; omit to use `main` branch for the latest features
         event = "VeryLazy",
         -- Optional: See `:h nvim-surround.configuration` and `:h nvim-surround.setup` for details
